2be5b50085970b934a154eb1777f651a849cc858
Vinícius <viniciusstorch62@gmail.com>
2025-12-02 08:43:44 -0300

    Início da construção do layout da tela de listagem

--- cadastro_usuarios.php
+++ cadastro_usuarios.php
@@ -1,3 +1,36 @@
-<?php
-    
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text" id="busca" name="busca">
+    <input type="submit" id="buscar" value="Buscar">
+    <br>
+
+    <h3>Listagem de Pessoas</h3>
+
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Data de Nasc.</th>
+            <th>Email</th>
+            <th>Telefone</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>João</td>
+            <td>2025/06/11</td>
+            <td>joao@gmail.com</td>
+            <td>(47) 9 9412-5363</td>
+            <td>
+                <input type="submit" id="editar" value="Editar">
+            </td>
+        </tr>
+    </table>
+
+</body>
+</html>
